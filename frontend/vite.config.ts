38aa49532c19a44e5a7a8ce4dc07fbf8a03e0203
FlyinPancake <36113055+FlyinPancake@users.noreply.github.com>
2026-03-12 11:50:22 +0100

    perf: oxidize frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite"
-import { devtools } from "@tanstack/devtools-vite"
-import { tanstackStart } from "@tanstack/react-start/plugin/vite"
-import viteReact from "@vitejs/plugin-react"
-import viteTsConfigPaths from "vite-tsconfig-paths"
-import tailwindcss from "@tailwindcss/vite"
-import { nitro } from "nitro/vite"
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import viteTsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import { nitro } from "nitro/vite";
 
 const config = defineConfig({
   plugins: [
@@ -18,6 +18,13 @@ const config = defineConfig({
     tanstackStart(),
     viteReact(),
   ],
-})
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+      },
+    },
+  },
+});
 
-export default config
+export default config;
