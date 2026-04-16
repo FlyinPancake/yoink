eeedfaf8365ffc0f86854be1818328377f068292
FlyinPancake <36113055+FlyinPancake@users.noreply.github.com>
2026-04-16 10:54:00 +0100

    fix: re-add dotenv support (#40)

--- crates/yoink-server/src/main.rs
+++ crates/yoink-server/src/main.rs
@@ -64,6 +64,7 @@ struct ApiDoc;
 
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
     let app_config = AppConfig::from_env().unwrap_or_else(|err| {
         panic!("Failed to parse configuration from environment: {err}");
     });
