c92f0756939d7bf3b30d3b4b9ee27edb72eb3e27
FlyinPancake <36113055+FlyinPancake@users.noreply.github.com>
2026-04-17 09:12:23 +0100

    build(deps): update frontend dependencies (#51)
    * style: reformat vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,20 +2,13 @@ import { defineConfig } from "vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 const config = defineConfig({
-  plugins: [
-    devtools(),
-    tanstackRouter(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
-    tailwindcss(),
-    viteReact(),
-  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  plugins: [devtools(), tanstackRouter(), tailwindcss(), viteReact()],
   server: {
     proxy: {
       "/api": "http://localhost:3000",
